14c10cfd7c0e3586364206a09b94923eaee81dc2
Rahul Kumar <nirajrahul1@gmail.com>
2025-04-06 20:47:09 +0530

    Add PATCH /movies/{id} to update posterPath

--- src/main/java/com/niraj/movieticketbooking/MovieController.java
+++ src/main/java/com/niraj/movieticketbooking/MovieController.java
@@ -1,9 +1,11 @@
 package com.niraj.movieticketbooking;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -45,5 +47,14 @@ public class MovieController {
         }
         movieRepository.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+    @PatchMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie updates) {
+        Movie movie = movieRepository.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Movie not found"));
+        if (updates.getPosterPath() != null) movie.setPosterPath(updates.getPosterPath());
+        Movie updatedMovie = movieRepository.save(movie);
+        return ResponseEntity.ok(updatedMovie);
     }
 }
