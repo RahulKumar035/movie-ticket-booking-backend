0be95ceb2160a30838bccdd4ee96305bcfcebcf1
Rahul Kumar <nirajrahul1@gmail.com>
2025-04-06 18:15:04 +0530

    Update Dockerfile and add debug logging for DB credentials

--- src/main/java/com/niraj/movieticketbooking/MovieTicketBookingApplication.java
+++ src/main/java/com/niraj/movieticketbooking/MovieTicketBookingApplication.java
@@ -1,17 +1,18 @@
 package com.niraj.movieticketbooking;
 
-import org.springframework.boot.CommandLineRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class MovieTicketBookingApplication {
+    private static final Logger logger = LoggerFactory.getLogger(MovieTicketBookingApplication.class);
 
     public static void main(String[] args) {
+        logger.info("DB_URL from env: {}", System.getenv("DB_URL"));
+        logger.info("DB_USERNAME from env: {}", System.getenv("DB_USERNAME"));
+        logger.info("DB_PASSWORD from env: {}", System.getenv("DB_PASSWORD"));
         SpringApplication.run(MovieTicketBookingApplication.class, args);
     }
-
-
-        }
+}
